156f794f6d5a20f406a164dac075a4b122a6a803
Jair Yara <jair.yara11@gmail.com>
2024-09-17 07:05:02 -0500

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'clase-17-09',
+  webDir: 'dist'
+};
+
+export default config;
